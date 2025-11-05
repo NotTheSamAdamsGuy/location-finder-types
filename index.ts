5c0e1ab070ddf9e461052e8499931d9ec067db68
Jim Koch <jimkoch52@gmail.com>
2025-11-05 15:25:52 -0800

    initial commit

---
+++ index.ts
@@ -0,0 +1,35 @@
+import { Feature, FeatureCollection } from "geojson";
+
+export interface LocationFeature extends Feature {
+  properties: {
+    name: string;
+    description: string;
+    address: string;
+    city: string;
+    state: {
+      name: string;
+      abbreviation: string;
+    };
+    postalCode: string;
+    country: {
+      name: string;
+      countryCode: string;
+    };
+    coordinates: {
+      longitude: number;
+      latitude: number;
+    };
+    images?: LocationImage[];
+    tags?: string[];
+  };
+}
+
+export interface LocationFeatureCollection extends FeatureCollection {
+  features: LocationFeature[];
+}
+
+export interface LocationImage {
+  originalFilename: string;
+  filename: string;
+  description?: string;
+}
