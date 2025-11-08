b4446812e463333da7c8d3c0a48e734f03290dab
Jim Koch <jimkoch52@gmail.com>
2025-11-08 13:44:23 -0800

    type updates

--- index.ts
+++ index.ts
@@ -41,6 +41,52 @@ export type LocationImage = {
   description?: string;
 };
 
+type MapboxContextBase = {
+  id: string;
+  name: string;
+};
+
+type MapboxContext = {
+  country?: MapboxContextBase & {
+    country_code: string;
+    country_code_alpha_3: string;
+  };
+  region?: MapboxContextBase & {
+    region_code: string;
+    region_code_full: string;
+  };
+  postcode?: MapboxContextBase;
+  district?: MapboxContextBase;
+  place?: MapboxContextBase;
+  locality?: MapboxContextBase;
+  neighborhood?: MapboxContextBase;
+  address?: MapboxContextBase & {
+    address_number: string;
+    street_name: string;
+  };
+  street?: MapboxContextBase;
+};
+
+export type MapboxSuggestion = {
+  name: string;
+  name_preferred?: string;
+  mapbox_id: string;
+  feature_type: string;
+  address?: string;
+  full_address?: string;
+  place_formatted: string;
+  context: MapboxContext;
+  language: string;
+  maki?: string;
+  poi_category?: string[];
+  poi_category_ids?: string[];
+};
+
+export type MapSearchReply = {
+  suggestions: MapboxSuggestion[];
+  sessionToken: string;
+};
+
 export type UserProfile = {
   username: string;
   firstName: string;
